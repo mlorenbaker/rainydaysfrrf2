3ca909a627e2a5ea9ffafb34cf3dc3699d211a24
Maggie Baker <mb203@rice.edu>
2025-02-27 10:29:56 -0600

    additions

--- R/Photophys_functions.R
+++ R/Photophys_functions.R
@@ -331,16 +331,37 @@ get.F <- function(frrf) {
   F.vector <- rep(NA, length(frrf))  # Initialize with NA
 
   for (i in seq_along(frrf)) {
-    if (!is.null(frrf[[i]]$A) && "E" %in% names(frrf[[i]]$A) && "Fo" %in% names(frrf[[i]]$A)) {
+    if (!is.null(frrf[[i]]$A) && "E" %in% names(frrf[[i]]$A) && "F" %in% names(frrf[[i]]$A)) {
       index <- which(frrf[[i]]$A$E == 0)  # Find index where E == 0
 
       if (length(index) > 0) {  # Ensure index exists
-        Fo.vector[i] <- frrf[[i]]$A$`F`[index]  # Case-sensitive check
+        F.vector[i] <- frrf[[i]]$A$`F`[index]  # Case-sensitive check
       }
     }
   }
 
   return(F.vector)
+}
+
+#'LabSTAF Fv/Fm
+#' @title Extract Fv/Fm LabSTAF
+#' @param frrf A list of frrf data with components for extracting parameters
+#' @return Fq/Fm
+#' @export
+get.F <- function(frrf) {
+  fqfm.vector <- rep(NA, length(frrf))  # Initialize with NA
+
+  for (i in seq_along(frrf)) {
+    if (!is.null(frrf[[i]]$A) && "E" %in% names(frrf[[i]]$A) && "Fq/Fm" %in% names(frrf[[i]]$A)) {
+      index <- which(frrf[[i]]$A$E == 0)  # Find index where E == 0
+
+      if (length(index) > 0) {  # Ensure index exists
+        fqfm.vector[i] <- frrf[[i]]$A$Fq.Fm[index]  # Case-sensitive check
+      }
+    }
+  }
+
+  return(fqfm.vector)
 }
 
 
